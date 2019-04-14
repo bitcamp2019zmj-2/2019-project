2cc489d4d749f3e2f39e257c570f8a63aacf0a9f
jkjon <jkjon@DESKTOP-8R3KFMU>
2019-04-13 22:35:32 -0400

    image possibly for login + updated controller for login

--- BCJava/src/main/java/bc2019/zmj2/gui/FXMLController.java
+++ BCJava/src/main/java/bc2019/zmj2/gui/FXMLController.java
@@ -5,15 +5,25 @@ package bc2019.zmj2.gui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class FXMLController {
+
+	@FXML
+    private PasswordField txt_password;
 
     @FXML
     private TextField txt_firstName;
 
     @FXML
-    private Button btn_showName;
+    private Button enterButton;
+
+    @FXML
+    private ImageView imageLogin;
+    Image image1 = new Image(MainAppx.class.getResourceAsStream("lights.png"));
 
     @FXML
     void onClick_showName(ActionEvent event) {
