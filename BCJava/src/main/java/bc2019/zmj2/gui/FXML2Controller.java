7723d9d985c5ec2bdd45486dfce970c8094a76d5
jkjon <jkjon@DESKTOP-8R3KFMU>
2019-04-14 02:30:21 -0400

    new controller and updated path for signUpFXML.fxml

---
+++ BCJava/src/main/java/bc2019/zmj2/gui/FXML2Controller.java
@@ -0,0 +1,32 @@
+package bc2019.zmj2.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+
+
+public class FXML2Controller {
+	
+
+
+	    @FXML
+	    private AnchorPane Rgst;
+
+	    @FXML
+	    private TextField SignUpVerify_txtbox;
+
+	    @FXML
+	    private TextField signUpPass_txtbox;
+
+	    @FXML
+	    private TextField signUpEmailAddress_txtbox;
+
+	    @FXML
+	    private Button RegisterButton;
+
+	}
+
+
+
