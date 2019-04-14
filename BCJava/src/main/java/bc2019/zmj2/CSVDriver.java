f7b7cd171ac7b3df8981d46a640bc925ce03f0e6
Morgan <quixoticbibliomania@gmail.com>
2019-04-14 01:38:18 -0400

    pretending to do work

--- BCJava/src/main/java/bc2019/zmj2/CSVDriver.java
+++ BCJava/src/main/java/bc2019/zmj2/CSVDriver.java
@@ -7,7 +7,6 @@ public class CSVDriver {
 	public static void main(String[] args) {
 		Database.getFromCSV("CMSC");
 		System.out.println(Database.getCourse("CMSC250").getDescription());
-		
 	}
 
 }
